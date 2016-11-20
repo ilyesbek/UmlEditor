e0e65d674007bf731faf683d0ce116dd77f515e4
yacine benaffane <yacine benaffane>
2016-11-20 23:33:25 +0100

    Initialize

---
+++ src/UmlEditor/Editor.java
@@ -0,0 +1,55 @@
+package UmlEditor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Editor extends JPanel {
+	
+	//BONJOUR// 
+	//SALUT//
+   private JLabel labelWelcome;
+   
+	private ImageIcon iconBackground;
+	private Image picture;
+	
+	private int xFond;
+	
+	public Editor(){
+		
+		super();
+		
+		this.proprietesEditeur();
+		
+		this.xFond = -50;
+		
+		iconBackground = new ImageIcon(getClass().getResource("/image/background.png"));
+		this.picture=this.iconBackground.getImage();
+	}
+	
+	
+private void proprietesEditeur(){
+		
+		this.setLayout(null);
+		this.proprietesEtiquette();
+	}
+	
+	
+	private void proprietesEtiquette(){
+		
+		labelWelcome = new JLabel();
+		this.labelWelcome.setBounds(200, 200, 700, 50);
+		this.labelWelcome.setText("Bienvenue dans votre editeur UML Pour commencer un nouveau projet,cliquer sur File,New,New UML project ");
+		this.add(labelWelcome);
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		Graphics g2 = (Graphics2D)g;
+		
+		g2.drawImage(this.picture, this.xFond, 0, null);
+	}
+}
---
+++ src/UmlEditor/Main.java
@@ -0,0 +1,60 @@
+package UmlEditor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Main {
+	
+	public static Editor editor;
+
+	public static void main(String[] args) {
+		JMenuBar menubar = new JMenuBar();
+		
+		JFrame fenetre = new JFrame("Editeur UML"); // le nom de l'application
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //quand on ferme la fenetre, le programme s'arrete 
+		fenetre.setSize(1000, 600);// dimension de la fenetre
+		fenetre.setLocationRelativeTo(null);// pour que la fenetre soit centré
+		fenetre.setResizable(false);// pour redimensioner la fenetre
+		fenetre.setAlwaysOnTop(true);// pour que la fenetre reste au dessus des autres fenetre
+		
+
+		JMenu file = new JMenu("File");
+		menubar.add(file);
+		JMenuItem newproject = new JMenuItem("New Project");
+		file.add(newproject);
+		JMenuItem save = new JMenuItem("Save");
+		file.add(save);
+		JMenuItem exit = new JMenuItem("Exit");
+		file.add(exit);
+		
+		JMenu edit = new JMenu("Edit");
+		menubar.add(edit);
+		
+		JMenu view = new JMenu("View");
+		menubar.add(view);
+		
+		JMenu help = new JMenu("Help");
+		menubar.add(help);
+		JMenuItem about = new JMenuItem("About");
+		help.add(about);
+		
+		
+		class exitaction implements ActionListener{
+			public void actionPerformed (ActionEvent e){
+				System.exit(0);
+			}
+		}
+		exit.addActionListener(new exitaction());
+		
+		editor = new Editor();
+		
+		  fenetre.setContentPane(editor); //on associe la scene à la fenetre de l'application
+	      fenetre.setVisible(true);// pour que la fenetre soit visible
+	      fenetre.setJMenuBar(menubar);
+	}
+
+}
